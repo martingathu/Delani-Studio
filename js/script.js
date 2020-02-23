683a57da8e6a23bbf2d0b946bca1c2b12eb8767d
martingathu <martin5gathu@gmail.com>
2020-02-23 16:50:26 +0300

    add toggle effect on what we do classes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
-    $("#design").click(function(){
+    $(".design").click(function(){
         $("#design-p").toggle();
+        $("#design").toggle();
     });
-    $("#dev").click(function(){
+    $(".development").click(function(){
         $("#dev-p").toggle();
+        $("#dev").toggle();
     });
-    $("#product").click(function(){
+    $(".product").click(function(){
         $("#product-p").toggle();
+        $("#product").toggle();
      });
     
      $(".img_wrap").hover(function () {
