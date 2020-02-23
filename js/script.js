d4e24988f743308d580c3e6a7ac069fe555c8a40
martingathu <martin5gathu@gmail.com>
2020-02-23 12:42:22 +0300

    create a click event for the contact form

--- js/script.js
+++ js/script.js
@@ -8,4 +8,22 @@ $(document).ready(function(){
     $("#product").click(function(){
         $("#product-p").toggle();
      });
+
+    $("#submit").click(function(){      
+      var name = $("#names").val();
+      var email = $("#email").val();
+      var message = $("#message").val();
+
+        if (name == '' || email == '' || message == ''){
+          $("#post").text("Please fill all fields");
+          $(".post").css('background-color', 'red')
+        } else {
+          $("#post").text(name + " we have received your message. Thank you for reaching out to us");
+          $(".post").css('background-color', 'black')
+          $("form")[0].reset();
+          
+        }
+        event.preventDefault();
+      });
+   
 });
