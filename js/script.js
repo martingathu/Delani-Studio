6ad78ba87c04b7d2032e3446212f69333e920512
martingathu <martin5gathu@gmail.com>
2020-02-23 15:48:27 +0300

    user information validation on contacts form

--- js/script.js
+++ js/script.js
@@ -8,22 +8,24 @@ $(document).ready(function(){
     $("#product").click(function(){
         $("#product-p").toggle();
      });
-
-    $("#submit").click(function(){      
-      var name = $("#names").val();
+    
+     $(".img_wrap").hover(function () {
+      $(this).find("div").toggleClass("img_description_show");
+    });
+    
+    $("#submit").click(function () {
+      event.preventDefault()
+      var name = $("#name").val();
       var email = $("#email").val();
       var message = $("#message").val();
 
-        if (name == '' || email == '' || message == ''){
-          $("#post").text("Please fill all fields");
-          $(".post").css('background-color', 'red')
-        } else {
-          $("#post").text(name + " we have received your message. Thank you for reaching out to us");
-          $(".post").css('background-color', 'black')
-          $("form")[0].reset();
-          
-        }
-        event.preventDefault();
-      });
-   
+      if (name == '' || email == '' || message == '') {
+        $("#post").html("Please fill all fields");
+        $(".post").css('background-color', 'red')
+      } else {
+        $("#post").html(name + " we have received your message. Thank you for reaching out to us")
+        $(".post").css('background-color', 'green')
+        $("form")[0].reset();
+      }
+    });
 });
